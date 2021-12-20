62b6b173cb4afa5b640b844393d022a6b2b565e4
rlacksgus97 <rlacksgus97@naver.com>
2021-12-20 18:17:51 +0900

    2021-12-20 X

---
+++ 2021-12/1966.py
@@ -0,0 +1,18 @@
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    priority = list(map(int, input().split()))
+    priority = [(i, idx) for idx, i in enumerate(priority)]
+
+    count = 0
+    while True:
+        if priority[0][0] == max(priority, key=lambda x: x[0])[0]:
+            count += 1
+            if priority[0][1] == m:
+                print(count)
+                break
+            else:
+                priority.pop(0)
+        else:
+            priority.append(priority.pop(0))
