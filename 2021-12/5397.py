bad3dd3cf7a4a593e7b735833fa79e9599255096
rlacksgus97 <rlacksgus97@naver.com>
2021-12-20 18:47:35 +0900

    2021-12-20 X

---
+++ 2021-12/5397.py
@@ -0,0 +1,21 @@
+t = int(input())
+
+for _ in range(t):
+    a = input()
+    left = []
+    right = []
+
+    for x in a:
+        if x == '<':
+            if left != []:
+                right.append(left.pop())
+        elif x == '>':
+            if right != []:
+                left.append(right.pop())
+        elif x == '-':
+            if left != []:
+                left.pop()
+        else:
+            left.append(x)
+    left.extend(reversed(right))
+    print(''.join(left))
