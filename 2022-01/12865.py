7301a7cf970779531eda04104eea2e4aebe589dd
rlacksgus97 <rlacksgus97@naver.com>
2022-01-25 11:23:25 +0900

    2022-01-25

--- 2022-01/12865.py
+++ 2022-01/12865.py
@@ -1,6 +1,15 @@
+#Knapsack Problem
+#fail
+
 n, k = map(int, input().split())
-wv = []
-v = []
-for _ in range(n):
-    wv.append(tuple(map(int, input().split())))
-wv.sort()
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+    w, v = map(int, input().split())
+    for j in range(1, k+1):
+        if w > j:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i][j-w]+v, dp[i-1][j])
+
+print(dp[-1][-1])
