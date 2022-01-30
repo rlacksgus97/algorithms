50af005ff751bb752a0110650bffb5c1f4d946d6
rlacksgus97 <rlacksgus97@naver.com>
2022-01-30 18:33:51 +0900

    2022-01-30

--- 2021-12/1092.py
+++ 2021-12/1092.py
@@ -1,7 +1,12 @@
+#fail
+
 n = int(input())
 cranes = list(map(int, input().split()))
 m = int(input())
 boxes = list(map(int, input().split()))
+
+pick = [0 for _ in range(n)]
+shipped = [False for _ in range(m)]
 
 cranes.sort(reverse=True)
 boxes.sort(reverse=True)
@@ -10,25 +15,17 @@ if cranes[0] < boxes[0]:
     print(-1)
     exit(0)
 
-pointer = [0]*n
-check = [False]*m
-
 time = 0
 count = 0
-
 while True:
     if count == m:
         break
     for i in range(n):
-        while pointer[i] < m:
-            if not check[pointer[i]] and cranes[i] >= boxes[pointer[i]]:
-                check[pointer[i]] = True
-                pointer[i] += 1
+        while pick[i] < m:
+            if not shipped[pick[i]] and cranes[i] >= boxes[pick[i]]:
+                shipped[pick[i]] = True
                 count += 1
                 break
-            pointer[i] += 1
+            pick[i] += 1
     time += 1
-
 print(time)
-
-#fail
