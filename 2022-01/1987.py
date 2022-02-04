3126f36e48eebc2d70f7f8a1ed08fbb5ed2d4b67
rlacksgus97 <rlacksgus97@naver.com>
2022-02-04 09:39:15 +0900

    2022-02-04

--- 2022-01/1987.py
+++ 2022-01/1987.py
@@ -1,19 +1,23 @@
+#가장 긴 이동 거리는 bfs로 푸는게 나을듯?
+
 r, c = map(int, input().split())
 board = []
 for _ in range(r):
     board.append(list(input()))
 
 answer = 1
-def rec_func(x, y, history, count):
+def rec_func(x, y, count):
     global answer
     for d in [(-1,0),(0,1),(1,0),(0,-1)]:
         nx = x + d[0]
         ny = y + d[1]
-        if 0<=nx<r and 0<=ny<c and board[nx][ny] not in history:
-            history.append(board[nx][ny])
-            rec_func(nx, ny, history, count+1)
-            history.remove(board[nx][ny])
+        if 0<=nx<r and 0<=ny<c and alphabet[ord(board[nx][ny])-ord('A')] == 0:
+            alphabet[ord(board[nx][ny])-ord('A')] = 1
+            rec_func(nx, ny, count+1)
+            alphabet[ord(board[nx][ny])-ord('A')] = 0
     answer = max(answer, count)
 
-rec_func(0, 0, [board[0][0]], 1)
+alphabet = [0 for _ in range(26)]
+alphabet[ord(board[0][0])-ord('A')] = 1
+rec_func(0, 0, 1)
 print(answer)
--- 2022-01/9663.py
+++ 2022-01/9663.py
@@ -1,31 +1,25 @@
 n = int(input())
 
-def attackable(row, col, board):
+def attackable(row, col):
     for r in range(row):
-        if board[r][col] == 1:
+        if board[r] == col:
             return False
-    for x in range(1, min(row, col)+1):
-        if (0 <= col-x and board[row-x][col-x] == 1) or (col+x < n and board[row-x][col+x] == 1):
+        if abs(board[r]-col) == abs(row-r):
             return False
     return True
 
 answer = 0
-def rec_func(row, board):
+def rec_func(row):
     global answer
     if row == n:
         answer += 1
-        # for i in range(n):
-        #     print(board[i])
-        # print()
         return
-    for col in range(n):
-        if attackable(row, col, board):
-            board[row][col] = 1
-            rec_func(row+1, board)
-            board[row][col] = 0
+    for c in range(n):
+        if attackable(row, c):
+            board[row] = c
+            rec_func(row+1)
+            # board[row] = -1
 
-for i in range(n):
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    board[0][i] = 1
-    rec_func(i, board)
+board = [-1 for _ in range(n)]
+rec_func(0)
 print(answer)
