94cdd6b8299fadf2c218d93f2bd838c55dc60d88
rlacksgus97 <33649903+rlacksgus97@users.noreply.github.com>
2022-09-03 12:38:08 +0900

    [Gold IV] Title: 부분합, Time: 152 ms, Memory: 41620 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,27 +1,18 @@
-N, S = map(int, input().split())
-numbers = list(map(int, input().split()))
-
-prefixSum = [0 for _ in range(N+1)]
-for i in range(1, N+1):
-    prefixSum[i] += prefixSum[i-1] + numbers[i-1]
-
-length = N+1
-l, r = 1, 1
+N, S = map(int,input().split())
+nums = list(map(int,input().split()))
+i, j = 0, 0
+s = nums[0]
+ans = 100001
+ 
 while True:
-    if prefixSum[r]-prefixSum[l-1] >= S:
-        length = min(length, r-l+1)
-        l += 1
+    if s >= S:
+        s -= nums[i]
+        ans = min(ans, j - i + 1)
+        i += 1
     else:
-        r += 1
-        if r > N:
-            r -= 1
+        j += 1
+        if j == N:
             break
-while l <= r:
-    l += 1
-    if prefixSum[r]-prefixSum[l-1] >= S:
-        length = min(length, r-l+1)
-
-if length == N+1:
-    print(0)
-else:
-    print(length)
+        s += nums[j]
+ 
+print(0) if ans == 100001 else print(ans)
